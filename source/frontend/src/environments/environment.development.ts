78f3840584778bc5540c2c0451885daa51fd911d
Florian Schick <florian.schick@schick-software.de>
2025-01-07 09:32:22 +0100

    Demo for 'authentik' added.

--- source/frontend/src/environments/environment.development.ts
+++ source/frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiBasePath: `${location.protocol}//${location.hostname}:5050/api`,
-  authority: `<authority URL, often corresponds to issuer URL>`,
-  clientId: '<client ID>',
+  authority: '<authentik application issuer URL>',
+  clientId: '<authentik provider client ID>',
   scopes: 'openid profile email offline_access',
 };
