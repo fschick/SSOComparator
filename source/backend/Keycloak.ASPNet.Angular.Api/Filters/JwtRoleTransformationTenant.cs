6e8366693e51c4b226a82c95c9ca0e693610d1c1
Florian Schick <florian.schick@schick-software.de>
2025-01-24 13:49:12 +0100

    Authorization via roles added.

---
+++ source/backend/Keycloak.ASPNet.Angular.Api/Filters/JwtRoleTransformationTenant.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Keycloak.ASPNet.Angular.Api.Filters;
+
+/// <summary>
+/// Transforms provider specific roles from tenant to default role claims.
+/// </summary>
+/// <seealso cref="IClaimsTransformation" />
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global", Justification = "Instantiated via DI.")]
+public class JwtRoleTransformationTenant : IClaimsTransformation
+{
+    /// <summary>
+    /// Provides a central transformation point to change the specified principal.
+    /// Note: this will be run on each AuthenticateAsync call, so its safer to
+    /// return a new ClaimsPrincipal if your transformation is not idempotent.
+    /// </summary>
+    /// <param name="principal">The <see cref="T:System.Security.Claims.ClaimsPrincipal" /> to transform.</param>
+    /// <returns>
+    /// The transformed principal.
+    /// </returns>
+    public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+    {
+        var result = principal.Clone();
+
+        // TODO: Implement provider specific transformation here.
+
+        return Task.FromResult(result);
+    }
+}
